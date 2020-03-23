8ae741aa7b8a7303e8ef8f588a9fb27561f04af8
victorAnumudu <anumuduchukwuebuka@gmail.com>
2020-03-23 21:45:46 +0100

    pushed my js task1

---
+++ script.js
@@ -0,0 +1,8 @@
+var myName = "Anumudu Chukwuebuka Victor";
+var courses = ["html", "CSS", "javaScript"];
+
+console.log("My name is " + myName + ", I'm taking " + courses);
+
+for(i = 1; i <= 200; i+=2){
+    console.log(i);
+}
